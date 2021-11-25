ff2548ae9c0386c562e4c8c3e062269149e8514e
Pavlin Ivanov <pavlinivanov9@gmail.com>
2021-11-25 17:14:56 +0200

    react-navigation

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import tw from 'tailwind-rn';
 
 export default function App() {
   return (
     <View style={tw('flex-1 justify-center items-center')}>
-      <Text>Open up App.js to start working on your app</Text>
+      <Text>Open up App.js to start working on your</Text>
+      <Button title='Click Me' />
       <StatusBar style="auto" />
     </View>
   );
