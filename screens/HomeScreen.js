6d474a7e562781a1e3d2c73777a33597919c047a
Pavlin Ivanov <pavlinivanov9@gmail.com>
2021-12-01 16:14:23 +0200

    styling Card

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,6 +7,7 @@ import {
   SafeAreaView,
   TouchableOpacity,
   Image,
+  StyleSheet,
 } from "react-native";
 import useAuth from "../hooks/useAuth";
 import tw from "tailwind-rn";
@@ -17,26 +18,26 @@ let DUMMY_DATA = [
   {
     firstName: "Mario",
     lastName: "Bros",
-    occupation: "Game Starr",
-    photoUrl: require('../assets/mario.jpg'),
+    job: "Game Starr",
+    photoUrl: require("../assets/mario.jpg"),
     age: "33",
-    id: 123
+    id: 123,
   },
   {
     firstName: "Woman",
     lastName: "Random",
-    occupation: "Fashion",
-    photoUrl: require('../assets/woman.jpg'),
+    job: "Fashion",
+    photoUrl: require("../assets/woman.jpg"),
     age: "22",
-    id: 456
+    id: 456,
   },
   {
     firstName: "John",
     lastName: "Travolta",
-    occupation: "Hero",
-    photoUrl: require('../assets/pic.png'),
+    job: "Hero",
+    photoUrl: require("../assets/pic.png"),
     age: "44",
-    id: 789
+    id: 789,
   },
 ];
 
@@ -70,13 +71,32 @@ const HomeScreen = () => {
           <Ionicons name="chatbubbles" size={30} color="black" />
         </TouchableOpacity>
       </View>
-      <View style={tw('flex-1 -mt-6')} >  
+      <View style={tw("flex-1 -mt-6")}>
         <Swiper
-          containerStyle={{backggroundColor: 'transparent'}}
+          cardIndex={0}
+          stackSize={5}
+          animateCardOpacity
+          containerStyle={{ backggroundColor: "transparent" }}
           cards={DUMMY_DATA}
           renderCard={(card) => (
-            <View key={card.id} style={tw('bg-white h-3/4 rounded-xl')} >
-              <Image source={card.photoUrl} style={{width: '100%', height: '100%'}}/>
+            <View key={card.id} style={tw("bg-white h-3/4 rounded-xl")}>
+              <Image
+                source={card.photoUrl}
+                style={tw("absolute top-0 h-full w-full rounded-xl")}
+              />
+              <View
+                style={[tw(
+                  "absolute bottom-0 bg-white w-full h-20 flex-row justify-between items-between px-6 py-2 rounded-b-xl"
+                ), styles.cardShadow]}
+              >
+                <View>
+                  <Text style={tw('text-xl font-bold')} >
+                    {card.firstName} {card.lastName}
+                  </Text>
+                  <Text>{card.job}</Text>
+                </View>
+                <Text style={tw('font-bold text-2xl')} >{card.age}</Text>
+              </View>
             </View>
           )}
         />
@@ -86,3 +106,16 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
+
+let styles = StyleSheet.create({
+  cardShadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2
+  }
+})
