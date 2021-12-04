8be5e8312a3705cc66cd22ea635550f67562fa3c
Pavlin Ivanov <pavlinivanov9@gmail.com>
2021-12-04 13:57:04 +0200

    setDoc

--- screens/ModalScreen.js
+++ screens/ModalScreen.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 import tw from "tailwind-rn";
 import useAuth from "../hooks/useAuth";
 import { LogBox } from "react-native";
-LogBox.ignoreAllLogs()
+import { setDoc, doc } from "@firebase/firestore";
+import { db } from "../firebase";
+import { useNavigation } from "@react-navigation/core";
+LogBox.ignoreAllLogs();
 
 const ModalScreen = () => {
   let { user } = useAuth();
+  let [image, setImage] = useState(null);
+  let [job, setJob] = useState(null);
+  let [age, setAge] = useState(null);
+
+  let navigation = useNavigation()
+
+  let incompleteForm = !image || !job || !age
+
+  let updateUserProfile = () => {
+    setDoc(doc(db, 'users', user.uid), {
+      id: user.uid,
+      displayName: user.displayName,
+      photoURL: image,
+      job: job,
+      age: age,
+      // timestamp: serverTimestamp()
+    }).then(() => {
+      navigation.navigate('Home')
+    }).catch(error => {
+      alert(error.message)
+    })
+  }
 
   return (
     <View style={tw("flex-1 pt-1 items-center")}>
@@ -22,22 +47,39 @@ const ModalScreen = () => {
         Step 1: The Profile Pic
       </Text>
       <TextInput
-        placeholder='Enter a Profile Pic URL'
+        value={image}
+        onChangeText={setImage}
+        style={tw("text-center text-xl pb-2")}
+        placeholder="Enter a Profile Pic URL"
       />
       <Text style={tw("text-center p-4 font-bold text-red-400")}>
         Step 2: The Job
       </Text>
       <TextInput
-        placeholder='Enter your occupation'
+        value={job}
+        onChangeText={setJob}
+        style={tw("text-center text-xl pb-2")}
+        placeholder="Enter your occupation"
       />
       <Text style={tw("text-center p-4 font-bold text-red-400")}>
         Step 3: The Age
       </Text>
       <TextInput
-        placeholder='Enter your Age'
+        value={age}
+        onChangeText={setAge}
+        style={tw("text-center text-xl pb-2")}
+        placeholder="Enter your Age"
+        maxLength={2}
+        keyboardType='numeric'
       />
-      <TouchableOpacity style={tw('w-64 p-3 absolute bottom-10 rounded-xl bg-red-400')} >
-        <Text style={tw('text-center text-white text-xl')} >Update Profile</Text>
+      <TouchableOpacity
+        onPress={updateUserProfile}
+        disabled={incompleteForm}
+        style={[tw("w-64 p-3 absolute bottom-10 rounded-xl"),
+          incompleteForm ? tw('bg-gray-400') : tw('bg-red-400')
+        ]}
+      >
+        <Text style={tw("text-center text-white text-xl")}>Update Profile</Text>
       </TouchableOpacity>
     </View>
   );
